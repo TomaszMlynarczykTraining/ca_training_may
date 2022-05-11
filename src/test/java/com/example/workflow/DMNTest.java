ac1e6f0f70af84b6b45fac9fd11c7165ed341d52
unknown <tomasz.feliks.mlynarczyk@gmail.com>
2022-05-11 14:18:26 +0200

    DMN Test

--- src/test/java/com/example/workflow/DMNTest.java
+++ src/test/java/com/example/workflow/DMNTest.java
@@ -1,28 +1,24 @@
 package com.example.workflow;
 
 import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
-import org.camunda.bpm.engine.DecisionService;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.variable.Variables;
 import org.camunda.bpm.spring.boot.starter.test.helper.AbstractProcessEngineRuleTest;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.Assert.assertEquals;
 
-@Deployment(resources = {"processes/diagram_3.bpmn"})
+@Deployment(resources = {"processes/diagram_1.dmn"})
 public class DMNTest extends AbstractProcessEngineRuleTest {
-
-    @Autowired
-    private DecisionService decisionService;
 
     @Test
     public void shouldExecuteHappyPath() {
-        DmnDecisionTableResult decisionResult = decisionService
-                .evaluateDecisionTableByKey("DMN_001", Variables.createVariables()
-                        .putValue("userType", "Worker")
-                        .putValue("userName", "Tomasz"));
 
-        assertEquals(true, decisionResult.getFirstResult().get("result"));
+        DmnDecisionTableResult dmnDecisionRuleResults = processEngine.getDecisionService().
+                evaluateDecisionTableByKey("Decision_0fjsi5x",
+                        Variables.createVariables().putValue("pizzaType", "OTHER"));
+
+        assertEquals("false", dmnDecisionRuleResults.getFirstResult().getSingleEntry());
+
     }
 }
